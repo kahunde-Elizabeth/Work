7238694bd520e9653fc90f8341b3a65539f394f1
kahunde-Elizabeth <kahundeliz7@gmail.com>
2025-02-05 11:02:36 +0300

    Add files via upload
    This is basically my own practice in javascript

---
+++ practise.js
@@ -0,0 +1,40 @@
+/*function multiplication(numberOne, numberTwo){
+        return numberOne*numberTwo;//return function
+}
+console.log(multiplication(23,5));
+
+function multiply(numberOne, numberTwo){
+      let product =numberOne*numberTwo;
+        console.log(product)//void function
+}
+multiply(23,5)
+const summation=(numberOne, numberTwo)=>{
+     console.log(`The summation is ${numberOne+numberTwo}`)
+}
+summation(2,3)*/
+
+// function summation(num1,num2){
+//         return num1+num2
+// }
+// console.log(summation(2,4))
+
+// function summation(num1,num2){
+//         let sum=num1+num2;
+//         console.log(`The saummation is ${sum}`)
+// }
+// summation(9,69)
+
+let years=20
+if (years<18){
+        console.log('Minor')
+}else if(years>=18){
+        console.log('Adult')
+}else{
+        console.log('invalid')
+}
+
+let r=0
+while(r<109){
+        console.log(r)
+        r+=1;
+}
